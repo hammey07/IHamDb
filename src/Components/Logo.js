cd1c6c597160e872ecf1dd449f5040b4ab97a408
hammey07 <hammey07.hs@gmail.com>
2025-03-23 22:57:48 +0000

    new logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,8 +1,10 @@
+import test from "../IHamDb.png";
 export function Logo() {
   return (
     <div className="logo">
-      <span role="img">🍿</span>
-      <h1>usePopcorn</h1>
+      <span role="img">
+        <img src={test} alt="IHamDb Logo" />
+      </span>
     </div>
   );
 }
