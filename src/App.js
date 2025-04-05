3a6c5a653c40b24b399d51861b2e83630d728daf
hammey07 <hammey07.hs@gmail.com>
2025-04-05 22:18:20 +0100

    potential issue with arrayh

--- src/App.js
+++ src/App.js
@@ -267,7 +267,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 }
 
 function WatchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  // const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+
+  const avgImdbRating = average(
+    Array.isArray(watched)
+      ? watched
+          .map((movie) => Number(movie.imdbRating))
+          .filter((r) => !isNaN(r))
+      : []
+  );
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
 
